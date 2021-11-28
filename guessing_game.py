4e38f1b71142308d2665535a4479ba6b91116f7e
Ornelle Chougourou <abouou-mathilda-ornelle.chougourou@edu.devinci.fr>
2021-11-28 10:05:00 +0100

    Affichage du nombre d'essais

--- guessing_game.py
+++ guessing_game.py
@@ -2,6 +2,7 @@
 import random
 def guess_loop():
     number_to_guess = random.randint(1,100)
+    essais = 0
     nom = input ("First,what is your name ?: ")
     print ("Okay",nom)
     print ("I have in mind a number in between 1 and 100, can you find it ?")
@@ -10,11 +11,14 @@ def guess_loop():
             guess = int(input())
             if guess > number_to_guess :
                  print ("The number to guess is lower")
+                 essais = essais + 1
             elif guess < number_to_guess :
                  print ("The number to guess is higher")
+                 essais = essais + 1
             else :
                  print ("You just found the number , it was indeed",guess)
-                 print ("Congratulations",nom)
+                 print ("Congratulations",nom,"You found it in",essais,"trials")
+                 
                  return
         except ValueError as err :
             print ("Invalid input,please enter an integer")
